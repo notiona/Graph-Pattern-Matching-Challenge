c0294aa2e1bd9f15bbaccb74889e24f1ea312929
notiona <notiona@snu.ac.kr>
2021-05-23 14:30:18 +0900

    Merge buildDAG and other func files

--- include/backtrack.h
+++ include/backtrack.h
@@ -18,6 +18,34 @@ class Backtrack {
   void PrintAllMatches(const Graph &data, const Graph &query, const CandidateSet &cs);
   void printEmbedding(const std::vector<Vertex> &embedding);
   bool isEmbedding(const std::vector<Vertex> &embedding, const Graph &data, const Graph &query);
+  void buildDAG(const Graph &G, const Graph &q);
+  int C_ini(const Graph &G, const Graph &q, Vertex u);
+  void backtracking(const CandidateSet &cs);
+  inline void initVisited(size_t n);
+
+  Vertex extendable();
+  Vertex* C_m(Vertex u);
+
+  static std::vector<std::pair<Vertex, Vertex>> M; // (partial) embedding. vector가 아니라 배열로 하는 게 나을 수도?
+  bool* visited; //buildDAG, backtracking에서 두 번 쓰임
+
+ private:
+  Vertex root;
+  size_t q_size;
+  std::vector<Vertex>* q_D; //query->DAG. adjacency list 형태로 저장
 };
+
+/**
+ * @brief backtrack.cc에서 반복해서 사용되는 visited[]를 처음 선언할 때 false로 초기화시켜주는 함수
+ * @param n size of array
+ * @return none
+ * @author Jinhyeong Kim
+ */
+inline void Backtrack::initVisited(size_t n){
+  visited = new bool[n];
+  for(int i=0; i<n; ++i){
+    visited[i]=false;
+  }
+}
 
 #endif  // BACKTRACK_H_
--- src/backtrack.cc
+++ src/backtrack.cc
@@ -4,6 +4,7 @@
  */
 
 #include "backtrack.h"
+#include <queue>
 
 Backtrack::Backtrack() {}
 Backtrack::~Backtrack() {}
@@ -22,6 +23,11 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query, const Can
     printEmbedding(embedding);
   
   std::cout << "is Embedding?: "<< (isEmbedding(embedding, data, query) ? "true" : "false") << "\n";
+
+  buildDAG(data, query);
+
+  initVisited(cs.GetCSSize());
+  // backtracking(cs);
 }
 
 /**
@@ -84,4 +90,158 @@ bool Backtrack::isEmbedding(const std::vector<Vertex> &embedding, const Graph &d
     }
   }
   return true;
+}
+
+/**
+ * @brief build DAG(q_D) from query
+ * @details 메서드의 맨 아래에 만들어진 DAG(adjacency_list 형태)를 출력하는 코드가 주석처리 돼있습니다. 
+ * @param G data graph
+ * @param q query graph
+ * @return none
+ * @author Jinhyeong Kim
+ */
+void Backtrack::buildDAG(const Graph &G, const Graph &q){
+  // Select root
+  // root = argmin |C_ini(u)|/degree(u)
+  q_size = q.GetNumVertices();
+  size_t deg = q.GetDegree(0);
+  double size_C_ini = C_ini(G, q, 0);
+  double min = deg / size_C_ini;
+  Vertex min_idx = 0;
+  for(int u=1; u < q_size; ++u){
+    deg = q.GetDegree(u);
+    size_C_ini = C_ini(G, q, u);
+    double val = deg / size_C_ini;
+    if (min > val) {
+      min = val;
+      min_idx = u;
+    }
+  }
+  root = min_idx;
+
+  // Traverse query in BFS order
+  // BFS에서 same level의 경우에 순서를 정하는 것은 아직 따로 구현하지 않았음(논문에는 있지만)
+  initVisited(q_size);
+  visited[root] = true;
+
+  Vertex* visit_order = new Vertex[q_size];
+  visit_order[root] = 0;
+
+  q_D = new std::vector<Vertex>[q_size]; // query->DAG. adjacency list 형태로 저장
+  std::queue<int> queue;
+  queue.push(root);
+
+  for(int i=q.GetNeighborStartOffset(root); i<q.GetNeighborEndOffset(root); ++i){
+    Vertex v = q.GetNeighbor(i); // vertex v adjacent to u
+    q_D[root].push_back(v);
+    }
+  Vertex cnt = 0;
+  while(!queue.empty()){
+    Vertex u = queue.front(); queue.pop();
+    for(int i=q.GetNeighborStartOffset(u); i<q.GetNeighborEndOffset(u); ++i){
+      Vertex v = q.GetNeighbor(i);
+      if (visited[v]==false) {
+        visited[v] = true;
+        queue.push(v);
+        visit_order[v] = ++cnt;
+      }
+    }
+  }
+
+  // Direct all edges from earlier to later visited vertices.
+  for(Vertex u=0; u < q_size; ++u){
+    if(u!=root){
+      for(int i=q.GetNeighborStartOffset(u); i < q.GetNeighborEndOffset(u); ++i){
+        Vertex v = q.GetNeighbor(i);
+        if(visit_order[u] < visit_order[v]){
+          q_D[u].push_back(v);
+        }
+      }
+    }
+  }
+
+  // DAG adjacency list 출력 (확인용)
+  // for(int i=0; i<q.GetNumVertices(); ++i){
+  //   std::cout<<i;
+  //   for(int j=0; j<q_D[i].size(); ++j){
+  //     std::cout << " " << q_D[i][j];
+  //   }
+  //   std::cout<<"\n";
+  // }
+}
+
+/**
+ * @brief get the size of initial CS of query node u.
+ * @details v is in C_ini(u), if label(v)=label(u) && degree(v)>=degree(u)
+ *          v: node of data graph, u: node of query graph
+ * @param G data graph
+ * @param q query graph
+ * @param u node of query graph
+ * @return size of C_ini(u)
+ * @author Jinhyeong Kim
+ */
+int Backtrack::C_ini(const Graph &G, const Graph &q, Vertex u){
+  int cnt = 0;
+  for(int v=0; v < G.GetNumVertices(); ++v){ 
+    if (G.GetLabel(v)==q.GetLabel(v) && G.GetDegree(v)>=q.GetDegree(u)){
+      ++cnt;
+    }
+  }
+  return cnt;
+}
+
+/**
+ * @brief Backtrack. <Algorithm 2> in the paper.
+ * @details 구현 안 한 부분은 주석 처리해놓고서 논문의 pseudo code를 그대로 적었습니다.
+   M, visited[]는 class 변수로 두어서 따로 parameter에 넣지 않았습니다. 물론 이게 제대로 작동하는 건지는 저도 모르니 마음대로 수정해주시면 감사합니다.
+ * @param cs CandidateSet 
+ * @return none
+ * @author Jinhyeong Kim
+ */
+void Backtrack::backtracking(const CandidateSet &cs){
+  if(sizeof(M) == q_size){
+    // do printEmbedding()
+  }
+  else if (sizeof(M)==0){
+    for(size_t i=0; i<cs.GetCandidateSize(root); ++i){
+      Vertex v = cs.GetCandidate(root, i);
+      // do M <- (root, v); 
+      visited[v] = true;
+      backtracking(cs);
+      visited[v] = false;
+
+    }
+  }
+  else{
+    Vertex u = extendable();
+      for(size_t j=0; j < sizeof(C_m(u)); ++j){
+        Vertex v = C_m(u)[j];
+        if (!visited[v]){
+          // do M' <- M U (u,v);
+          visited[v] = true;
+          backtracking(cs);
+          visited[v] = false;
+        }
+      }
+  }  
+}
+
+/**
+ * @author Jinhyeong Kim
+ */
+Vertex Backtrack::extendable(){
+ // return a extendable vertex
+ // (아래는 단지 에러 일으키지 않기 위해 넣은 것)
+ Vertex tmp = 0;
+ return tmp;
+}
+
+/**
+ * @author Jinhyeong Kim
+ */
+Vertex* Backtrack::C_m(Vertex u){
+  // return C_m(u), set of extendable candidates of u regarding partial embedding M
+  // Vertex들의 배열을 반환
+ // (아래는 단지 에러 일으키지 않기 위해 넣은 것)
+  return &root;
 }
